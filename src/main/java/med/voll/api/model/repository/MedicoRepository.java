0f9ac7b4abf367485f86cf422424edb46b4ba91f
Gustavo H <hoffmann.gusttavo@gmail.com>
2023-04-22 18:18:13 -0300

    - configuração de testes de repositories

--- src/main/java/med/voll/api/model/repository/MedicoRepository.java
+++ src/main/java/med/voll/api/model/repository/MedicoRepository.java
@@ -1,14 +1,30 @@
 package med.voll.api.model.repository;
 
+import med.voll.api.model.entity.medico.Especialidade;
 import med.voll.api.model.entity.medico.Medico;
 import med.voll.api.model.repository.generic.GenericCrudRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Repository
 public interface MedicoRepository extends GenericCrudRepository<Medico> {
 
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
+
+    @Query("""
+        select m from Medico m
+        where m.ativo = true and m.especialidade = :especialidade
+        order by rand()
+        limit 1
+    """)
+    Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade);
+
+
 
 }
---
+++ src/test/java/med/voll/api/model/repository/MedicoRepositoryTest.java
@@ -0,0 +1,39 @@
+package med.voll.api.model.repository;
+
+import jakarta.persistence.EntityManager;
+import med.voll.api.model.entity.medico.Especialidade;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+
+@DataJpaTest
+//configurar para usar o mesmo banco de dados e nao usar banco em memoria, tipo h2
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    @DisplayName("Deveria devolver null quando único médico cadastrado não está disponível na data")
+    void escolherMedicoAleatorioLivreNaData() {
+        var proximaSegunda = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10, 0);
+        var medico = repository.escolherMedicoAleatorioLivreNaData(Especialidade.CARDIOLOGIA);
+        Assertions.assertThat(medico).isNull();
+    }
+}
