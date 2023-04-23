2b973685112fc3a3004bca93dadc49ba709c2d17
Gustavo H <hoffmann.gusttavo@gmail.com>
2023-04-23 20:44:52 -0300

    - testes controller de médico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -23,36 +23,36 @@ public class MedicoController {
 
     @Operation(description = "Cadastrar Médico")
     @PostMapping
-    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder){
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
         var medicoCriado = service.save(new Medico(dados));
         var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medicoCriado.getId()).toUri();
-       return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medicoCriado));
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medicoCriado));
     }
 
     @Operation(description = "Atualizar Médico")
     @PutMapping
-    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = service.atualizarInformacoes(dados);
         return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     @Operation(description = "Remover Médico")
     @DeleteMapping("/{id}")
-    public ResponseEntity remover(@PathVariable Long id){
+    public ResponseEntity remover(@PathVariable Long id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
     @Operation(description = "Buscar Médico pelo id")
     @GetMapping("/{id}")
-    public ResponseEntity detalhar(@PathVariable Long id){
+    public ResponseEntity detalhar(@PathVariable Long id) {
         var medico = service.findById(id);
         return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     @Operation(description = "Buscar todos os médicos ativos")
     @GetMapping
-    public ResponseEntity<Page<DadosListagemMedico>> lista(@PageableDefault(size = 15, sort = {"nome"}) Pageable paginacao){
+    public ResponseEntity<Page<DadosListagemMedico>> lista(@PageableDefault(size = 15, sort = {"nome"}) Pageable paginacao) {
         var page = service.findAllByAtivoTrue(paginacao)
                 .map(DadosListagemMedico::new);
         return ResponseEntity.ok(page);
